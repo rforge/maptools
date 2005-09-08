5c289bba229a180180fefec3d8885cb418247ce4
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-09-08 20:28:36 +0000

    dotsinpolys begun
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maptools@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/dotsinpolys.R
+++ trunk/R/dotsinpolys.R
@@ -1,46 +1,36 @@
-# Copyright 2004 (c) Roger Bivand
+# Copyright 2004-5 (c) Roger Bivand
 
-dotsInPolys <- function(pl, x, f=csr) {
-    if (!inherits(pl, "polylist")) stop("not a polylist object")
-    if (length(pl) != length(x)) stop("different lengths")
+dotsInPolys <- function(pl, x, f="random", compatible=FALSE) {
+    if (!is.character(f)) stop("f must be a character string")
+    if (f != "random" && f != "regular") stop(paste(f, "not supported"))
+    if (inherits(pl, "polylist")) pl <- .polylist2SpP(pl)
+    if (!is(pl, "SpatialPolygons")) stop("unknown class of input polygons")
+    pls <- getSpPpolygonsSlot(pl)
+    IDs <- getSpPPolygonsIDSlots(pl)
+    if (length(pls) != length(x)) stop("different lengths")
     if (!inherits(x, "integer")) {
         x <- as.integer(x)
         warning("x coerced to integer")
     }
-    if (!require(splancs)) stop("splancs package required for this function")
-    n <- length(pl)
-    if (n < 1) stop("zero length polylist")
+    n <- length(pls)
+    if (n < 1) stop("zero Polygons")
     res <- vector(mode="list", length=n)
+    ID_out <- NULL
     for (i in 1:n) {
+cat("i", i, "\n")
         if (x[i] > 0) {
-            if (is.null(attr(pl[[i]], "nParts")) || 
-		attr(pl[[i]], "nParts") == 1) {
-                if (attr(pl[[i]], "ringDir") != 1)
-                    warning(paste("hole with content at:", i))
-                res[[i]] <- f(matrix(c(pl[[i]]), ncol=2), x[i])
-            } else if (length(attr(pl[[i]], "nParts")) > 1) {
-                areas <- rep(0, attr(pl[[i]], "nParts"))
-                for (j in 1:attr(pl[[i]], "nParts")) {
-                    if (attr(pl[[i]], "ringDir")[j] == 1) {
-                        from <- attr(pl[[i]], "pstart")$from[j]
-                        to <- attr(pl[[i]], "pstart")$to[j]
-                        areas[j] <- areapl(matrix(c(pl[[i]][from:to,]), 
-                            ncol=2))
-                    }
-                }
-                pareas <- areas/sum(areas)
-                px <- as.integer(round(pareas*x[i], digits = 0))
-                for (j in 1:attr(pl[[i]], "nParts")) {
-                    if (px[j] > 0) {
-                        from <- attr(pl[[i]], "pstart")$from[j]
-                        to <- attr(pl[[i]], "pstart")$to[j]
-                        pj <- matrix(c(pl[[i]][from:to,]), ncol=2)
-                        res[[i]] <- rbind(res[[i]], f(pj, px[j]))
-                    }
-                }
-                res[[i]] <- matrix(res[[i]], ncol=2)
-            }
-        }
+            res[[i]] <- sample.Polygons(pls[[i]], x[i], type=f)
+	    ID_out <- c(ID_out, IDs[i])
+	}
+    }
+    if (!compatible) {
+        resa <- do.call("rbind", lapply(res, function(x) 
+	    if(!is.null(x)) coordinates(x)))
+	reps <- unlist(sapply(res, function(x) 
+	    if(!is.null(x)) nrow(coordinates(x))))
+	res <- data.frame(resa, rep(ID_out, reps))
+	names(res) <- c("x", "y", "ID")
+	coordinates(res) <- c("x", "y")
     }
     res
 }
