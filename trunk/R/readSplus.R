88ef67549f8bd645c339bbb5f3cbb207a31ba506
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-05 15:39:22 +0000

    adding features to readSplus
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maptools@110 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/readSplus.R
+++ trunk/R/readSplus.R
@@ -11,21 +11,22 @@ readSplus<-function(file, proj4string=CRS(as.character(NA)) )
 	offset<-1
 	if(lns[2]==""){offset<-offset+1}
 
+        xscale <- 1
+        yscale <- 1
         scales <- grep("cale", lns[(offset+1):(offset+2)])
 	if (length(scales) > 0) {
             if (length(scales) < 2) 
                 stop("Only one scale given")
             xsc <- grep("x|X", lns[(offset+1):(offset+2)])
             ysc <- grep("y|Y", lns[(offset+1):(offset+2)])
-            xscale <- as.numeric(strsplit(lns[(offset+xsc)], 
-                " |\t|\r\n")[[1]][2])
-	    yscale <- as.numeric(strsplit(lns[(offset+ysc)],
-                " |\t|\r\n")[[1]][2])
+            xscale <- strsplit(lns[(offset+xsc)], ":| |\t|\r\n")[[1]]
+            if (any(xscale == "")) xscale <- xscale[-which(xscale == "")]
+            xscale <- as.numeric(xscale[2])
+            yscale <- strsplit(lns[(offset+xsc)], ":| |\t|\r\n")[[1]]
+            if (any(yscale == "")) yscale <- yscale[-which(yscale == "")]
+            yscale <- as.numeric(yscale[2])
             offset <- offset+2
 	    if(lns[(offset+1)]==""){offset<-offset+1}
-        } else {
-            xscale <- 1
-            yscale <- 1
         }
 
 	IDs<-lapply(lns[offset+1:nareas], function(X){strsplit(X, " |\t|\r\n")})
@@ -33,20 +34,33 @@ readSplus<-function(file, proj4string=CRS(as.character(NA)) )
 
 	offset<-offset+nareas
 
-	if(lns[offset]==""){offset<-offset+1}
+	if(lns[offset+1]==""){offset<-offset+1}
 
-        END <- 1
-        while (lns[nlines - (END-1)] != "END") END <- END+1
+        END <- which(lns == "END")
+        lns <- lns[(offset+1):(END-1)]
+        if (any(lns == "")) {
+            empty <- which(lns == "")
+            lns <- lns[-empty]
+        }
 
-	polys<-read.table(file, skip=offset, nrows=nlines-offset-END)
+#	polys<-read.table(file, skip=offset, nrows=nlines-offset-END)
+        polys <- lapply(lns, function(X){strsplit(X, " |\t|\r\n")})
+        polys <- matrix(unlist(polys), ncol=3, byrow=TRUE)
+        wNA <- which(polys[,1] == "NA")
+        is.na(polys[wNA,1]) <- TRUE
+        is.na(polys[wNA,2]) <- TRUE
+        is.na(polys[wNA,3]) <- TRUE
 
-	polys2<-cbind(xscale*as.numeric(polys$V2), yscale*as.numeric(polys$V3))
+	polys2<-cbind(xscale*as.numeric(polys[,2]),
+            yscale*as.numeric(polys[,3]))
 
 	lpolys<-.NAmat2xyList(polys2)
-	llpolys<-unlist(lapply(lpolys, nrow))
+#	llpolys<-unlist(lapply(lpolys, nrow))
 
-	idx<- c(1, cumsum(2+llpolys[-length(lpolys)]))
-	polysIDs<-polys$V1[ idx]
+#	idx<- c(1, cumsum(2+llpolys[-length(lpolys)]))
+        wNA <- which(is.na(polys[, 1]))
+        idx <- c(1, wNA+1)
+	polysIDs<-polys[idx, 1]
 
 	belongs<-lapply(1:nareas, function(i){which(polysIDs==IDs[i,2])})
 
